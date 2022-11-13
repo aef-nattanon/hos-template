b6c4c8525a5735163f4de77000f58fa44b4fe2d2
aefnattanon <aef.nattanon@gmail.com>
2022-11-13 21:56:05 +0700

    Fix

--- src/components/HandleUser.tsx
+++ src/components/HandleUser.tsx
@@ -1,12 +1,10 @@
-import { getCookie } from 'cookies-next';
 import React from 'react';
 
-import { COOKIE_OPTION } from '../constants';
-import { useRedirect } from '../hooks/commons';
+import { useCookie, useRedirect } from '../hooks/commons';
 import Loading from './Loading';
 
 export default function AppLayout({ children }) {
-  const user = getCookie("user", COOKIE_OPTION);
+  const [user] = useCookie("user", "");
   useRedirect("/sing-in", !user || user === "");
   if (user) {
     return <>{children}</>;
